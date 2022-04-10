368646d7f421a92b52614393a722adbdd7e75f6f
samstorment <sjstorment22@gmail.com>
2022-04-10 03:30:24 -0400

    Fixed session typing and profile redirect

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,18 @@
 /// <reference types="@sveltejs/kit" />
 
-import type { UserType } from "$lib/models/user.model";
 import type User from "$lib/models/user.model";
-
 
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	interface Session {
-		user: User
+
+// had to make this global after using an import
+declare global {
+	declare namespace App {
+		// interface Locals {}
+		// interface Platform {}
+		interface Session {
+			user: User
+		}
+		// interface Stuff {}
 	}
-	// interface Stuff {}
 }
--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,10 +1,5 @@
 <script lang="ts" context="module">
-    import { session } from "$app/stores";
-    import { dateTime } from "$lib/util";
-    import ProfileRow from "./_ProfileRow.svelte";
-
     export function load({ session }) {
-        console.log("load called");
 
 		if (!session.user) {
 			return {
@@ -17,21 +12,28 @@
 	}
 </script>
 
-<div id="profile">
-    <div id="profile-container">
-        <h1>Profile</h1>
-        
-        <ProfileRow label="Email" value={$session.user.email} />
-        <ProfileRow label="Username" value={$session.user.username} />
-        <ProfileRow label="Display Name" value={$session.user.displayName} />
-        <ProfileRow label="Last Sign In" value={dateTime($session.user.last_sign_in_at)} />
-        <ProfileRow label="Account Created At" value={dateTime($session.user.created_at)} />
-    </div>
-</div>
+<script>
+    import { session } from "$app/stores";
+    import { dateTime } from "$lib/util";
+    import ProfileRow from "./_ProfileRow.svelte";
+</script>
 
-<pre>
+{#if $session.user}
+    <div id="profile">
+        <div id="profile-container">
+            <h1>Profile</h1>
+            <ProfileRow label="Email" value={$session.user.email} />
+            <ProfileRow label="Username" value={$session.user.username} />
+            <ProfileRow label="Display Name" value={$session.user.displayName} />
+            <ProfileRow label="Last Sign In" value={dateTime($session.user.last_sign_in_at)} />
+            <ProfileRow label="Account Created At" value={dateTime($session.user.created_at)} />
+        </div>
+    </div>
+{/if}
+
+<!-- <pre>
     {JSON.stringify($session.user, null, 4)}
-</pre>
+</pre> -->
 
 <style>
     #profile {
